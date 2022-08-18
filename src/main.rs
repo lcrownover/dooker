f5523a5dbfb6c60a4c1e518fcdaaac17285abbf0
Lucas Crownover <lcrownover127@gmail.com>
2022-08-18 08:50:44 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use unicode_segmentation::UnicodeSegmentation;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   /// String to dookize
+   #[clap(short, long, value_parser)]
+   sentence: String,
+}
+
+fn dookize(s: &str) -> String {
+    let mut new_str = "".to_owned();
+    for g in s.graphemes(true) {
+        if g == "o" {
+            new_str.push_str("oo")
+        } else {
+            new_str.push_str(g)
+        }
+    }
+    return new_str
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{}", &dookize(&args.sentence))
+}
